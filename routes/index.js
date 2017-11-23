0e8e4ab1c30b68607ac6a61deba2ab95acbffade
tom <kebobftww@gmail.com>
2017-11-23 15:29:15 +0200

    Added endpoint to client

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 let nliController = require('../src/nli-gateway/controllers/nli-controller');
 
 /* GET home page. */
-router.get('/', nliController.get);
+router.get('/', (req, res, next) => res.render('index'));
+
+// API.
+router.get('/api/fetch', nliController.get);
 
 module.exports = router;
