bfd90957a183234eaacb32f1a6e643449c8226f2
gilgemer <gilgemer1@gmail.com>
2017-11-24 01:55:39 +0200

    Added keywords

--- src/nli-gateway/controllers/nli-controller.js
+++ src/nli-gateway/controllers/nli-controller.js
@@ -3,6 +3,9 @@
 const config = require('../../../config/config'),
       nli = require('../lib/nli-lib');
 
+let WordPOS = require('wordpos'),
+    wordpos = new WordPOS();
+
 module.exports = {
 
   get: function (req, res, next) {
@@ -16,21 +19,37 @@ module.exports = {
 
     function modifyResults(results) {
 
-      results = [].concat.apply([], results);
+        results = [].concat.apply([], results);
 
-      return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
 
-        results = results.map(function(result) {
-          result.title = result.title.replace(new RegExp('<br>', 'g'), '');
-          return result;
+            if (results.length === 0 || results[0] === undefined) {
+                return resolve([]);
+            }
+
+            results = results.map(function (result) {
+                result.title = result.title.replace(new RegExp('<br>', 'g'), '');
+                return result;
+            });
+
+            results = results.filter(function (result) {
+                return result.title.split(' ').length > 4;
+            });
+
+            let count = 0;
+            results.forEach(function (result, i) {
+                console.log(result.title);
+                wordpos.getNouns(result.title, function(data) {
+                    results[i].keywords = data;
+
+                    count++;
+                    if (count === results.length) {
+                      return resolve(results);
+                    }
+                });
+            });
+
         });
-
-        results = results.filter(function(result) {
-          return result.title.split(' ').length > 4;
-        });
-
-        resolve(results);
-      });
     }
 
     function returnResults(results) {
